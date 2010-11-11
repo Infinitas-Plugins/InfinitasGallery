4f0ca140cc41702b5ed172ed69283630fdc1c1a8
dogmatic69 <dogmatic69@gmail.com>
2010-11-11 14:48:01 +0000

    adding code for the new upload script, still need to make it work

--- models/image.php
+++ models/image.php
@@ -29,3 +29,55 @@
 	        )
 	    );
 	}
+
+/*
+	class Image extends GalleryAppModel{
+		var $name = 'Image';
+
+		public $virtualFields = array(
+			'image_path' => 'CONCAT("content/gallery/", Image.image)'
+		);
+
+		public $actsAs = array(
+	        'Filemanager.Upload' => array(
+	        	'image' => array(
+		        	'path' => 'plugins{DS}gallery{DS}webroot{DS}images{DS}',
+					'fields' => array(
+						'dir' => 'dir',
+						'type' => 'type',
+						'size' => 'size'
+					),
+					'thumbnails' => false,
+					'thumbsizes' => array(
+						'xvga' => '1024x768',
+						'vga' => '640x480',
+						'thumb' => '80x80'
+					),
+					'thumbnailQuality' => 80,
+					'thumbnailMethod' => 'php'
+				)
+	        )
+	    );
+
+		public function  __construct($id = false, $table = null, $ds = null) {
+			parent::__construct($id, $table, $ds);
+
+			$this->validate = array(
+				'image' => array(
+					'isCompletedUpload' => array(
+						'rule' => 'isCompletedUpload',
+						'message' => __('Image was not successfully uploaded', true)
+					),
+					'isFileUpload' => array(
+						'rule' => 'isFileUpload',
+						'message' => __('Image was missing from submission', true)
+					),
+					'isSuccessfulWrite' => array(
+						'rule' => 'isSuccessfulWrite',
+						'message' => __('Image could not be written to the server', true)
+					)
+				)
+			);
+		}
+	}
+*/
