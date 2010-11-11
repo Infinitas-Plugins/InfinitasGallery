faeccec5b72f50cc61c89972fcc2c02dc9c89164
dogmatic69 <dogmatic69@gmail.com>
2010-11-11 17:23:47 +0000

    creating a release for the gallery plugin

---
+++ config/releases/map.php
@@ -0,0 +1,6 @@
+<?php
+$map = array(
+	1 => array(
+		'000001_gallery' => 'R4cdc268621544bc390802a206318cd70'),
+);
+?>
---
+++ config/schema/schema.php
@@ -0,0 +1,33 @@
+<?php 
+/* SVN FILE: $Id$ */
+/* Gallery schema generated on: 2010-11-11 17:11:18 : 1289496198*/
+class GallerySchema extends CakeSchema {
+	var $name = 'Gallery';
+
+	function before($event = array()) {
+		return true;
+	}
+
+	function after($event = array()) {
+	}
+
+	var $images = array(
+		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'image' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'title' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'slug' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'description' => array('type' => 'text', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'keywords' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'dir' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 10, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'size' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 5),
+		'category_id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'views' => array('type' => 'integer', 'null' => false, 'default' => '0'),
+		'active' => array('type' => 'boolean', 'null' => false, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+}
+?>
