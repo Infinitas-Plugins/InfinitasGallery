00af26ec6cf5bc0a5239303a4447fcae4ecbfc36
dogmatic69 <dogmatic69@gmail.com>
2012-02-15 16:06:04 +0000

    removing the filter helper as it is loaded by the core

--- Controller/GalleryAppController.php
+++ Controller/GalleryAppController.php
@@ -1,22 +1,4 @@
 <?php
 	class GalleryAppController extends AppController {
-		/**
-		 * components.
-		 *
-		 * @access public
-		 * @var array
-		 */
-		public $components = array(
-			'Filter.Filter'
-		);
-
-		/**
-		 * Helpers.
-		 *
-		 * @access public
-		 * @var array
-		 */
-		public $helpers = array(
-			'Filter.Filter'
-		);
+		
 	}
