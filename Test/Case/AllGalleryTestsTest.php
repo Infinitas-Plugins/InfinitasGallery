e31b0ff27fcda2e1062efa5d3c574da941aeb09f
dogmatic69 <dogmatic69@gmail.com>
2012-09-29 23:45:15 +0100

    adding missing group test

---
+++ Test/Case/AllGalleryTestsTest.php
@@ -0,0 +1,17 @@
+<?php
+class AllGalleryTestsTest extends PHPUnit_Framework_TestSuite {
+
+/**
+ * Suite define the tests for this suite
+ *
+ * @return void
+ */
+	public static function suite() {
+		$suite = new CakeTestSuite('All Gallery test');
+
+		$path = CakePlugin::path('Gallery') . 'Test' . DS . 'Case' . DS;
+		$suite->addTestDirectoryRecursive($path);
+
+		return $suite;
+	}
+}
