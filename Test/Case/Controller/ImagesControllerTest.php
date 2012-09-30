d05d337815f0c2af9cf273127391dc4f73baaf90
dogmatic69 <dogmatic69@gmail.com>
2012-09-30 14:23:21 +0100

    new tests

---
+++ Test/Case/Controller/ImagesControllerTest.php
@@ -0,0 +1,67 @@
+<?php
+App::uses('ImagesController', 'Gallery.Controller');
+
+/**
+ * CmsContentsController Test Case
+ *
+ */
+class ImagesControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.gallery.image'
+	);
+
+/**
+ * testIndex method
+ *
+ * @return void
+ */
+	public function testIndex() {
+	}
+
+/**
+ * testView method
+ *
+ * @return void
+ */
+	public function testView() {
+	}
+
+/**
+ * testAdminIndex method
+ *
+ * @return void
+ */
+	public function testAdminIndex() {
+	}
+
+/**
+ * testAdminView method
+ *
+ * @return void
+ */
+	public function testAdminView() {
+	}
+
+/**
+ * testAdminAdd method
+ *
+ * @return void
+ */
+	public function testAdminAdd() {
+	}
+
+/**
+ * testAdminEdit method
+ *
+ * @return void
+ */
+	public function testAdminEdit() {
+	}
+
+}
