a79c4f12080a733688490a696c90084a65e0b367
dogmatic69 <dogmatic69@gmail.com>
2013-01-21 06:51:16 +0000

    adding coverage filter

--- Test/Case/AllGalleryTestsTest.php
+++ Test/Case/AllGalleryTestsTest.php
@@ -1,5 +1,7 @@
 <?php
-class AllGalleryTestsTest extends PHPUnit_Framework_TestSuite {
+App::uses('AllTestsBase', 'Test/Lib');
+
+class AllGalleryTestsTest extends AllTestsBase {
 
 /**
  * Suite define the tests for this suite
